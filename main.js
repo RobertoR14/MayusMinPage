e63cf7a91918a4ceed125fd631b552644fd6dbb8
Roberto Rodriguez <roberticor05@gmail.com>
2022-01-27 13:14:12 -0400

    v0.1

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let inp1 = document.querySelector('.inp-1');
 let inp2 = document.querySelector('.inp-2');
-let btnMayus = document.querySelector('.mayus');
-let btnMin = document.querySelector('.min');
+
+let btnSwitch = document.querySelector('#switch');
+let body = document.body;
+
+
 
 let newText;
 inp1.addEventListener("keyup", (event) => {
@@ -26,3 +29,26 @@ function ConvertMin(){
 
 }
 
+function Capitalize(){
+  inp2.innerHTML = newText[0].toUpperCase() + newText.substring(1);
+}
+
+//DARKMODE CODE
+
+btnSwitch.addEventListener('click', () => {
+  let darkMode = body.classList.toggle('dark');
+
+  btnSwitch.classList.toggle('active');
+
+  localStorage.setItem('switch', darkMode)
+});
+
+let d = localStorage.getItem('switch');
+
+ if(d == 'true'){
+  body.classList.add('dark');
+  btnSwitch.classList.add('active');
+} else {
+  body.classList.remove('dark');
+  btnSwitch.classList.remove('active');
+} 
